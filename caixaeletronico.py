f69e8d4511fefcf6f3a332c68f1a4f86e793df2a
Paiva-Destkop <37738982+LucasPaivaSilva@users.noreply.github.com>
2021-05-20 22:37:32 -0300

    Otimizações e Comentários

--- caixaeletronico.py
+++ caixaeletronico.py
@@ -1,62 +1,58 @@
 class CaixaEletronico:
+    # Construtor da classe
     def __init__(self):
-        self.Notas = {10: 5, 20: 1, 50: 0, 100: 0}
+        # Criação de um dicionário para armazenar as celulas do sistema e as quantidades das mesmas
+        self.Notas = {10: 0, 20: 0, 50: 0, 100: 0}
 
+    # Método para carregar as notas
     def carregarNotas(self, ValorDaNota, QuantidadeDeNotas):
         if (ValorDaNota in self.Notas and QuantidadeDeNotas != 0):
             self.Notas[ValorDaNota] += QuantidadeDeNotas
-            print(QuantidadeDeNotas, "notas de", ValorDaNota, "foram abastecidas")
+            print(QuantidadeDeNotas, "nota(s) de", ValorDaNota, "foram abastecidas")
         else:
-            print("Valor da nota inválido e/ou Tentativa de abastecer 0 notas")
+            print("Valor da nota inválido e/ou Tentativa de abastecer 0 nota(s)")
 
+    # Método para retornar a quantidade de dinheiro no caixa
     def retornarValorEmCaixa(self):
         ValorEmCaixa = 0
         for ValorDaNota in self.Notas:
             ValorEmCaixa += ValorDaNota * self.Notas[ValorDaNota]
         return ValorEmCaixa
 
+    # Método mostrar a quantidade de cada uma das notas presentes no caixa
     def mostrarCedulasEmCaixa(self):
         for ValorDaNota in self.Notas:
-            print(self.Notas[ValorDaNota], "notas de", ValorDaNota)
+            print(self.Notas[ValorDaNota], "nota(s) de", ValorDaNota)
 
+    # Método para sacar as notas
     def sacarNotas(self, ValorASerSacado):
         if ValorASerSacado > self.retornarValorEmCaixa():
             print("Saque negado! Quantidade insuficiente em caixa")      
         else:
+            # Criação de uma cópia do dicionário, feito para não interferir com a princípal em certas lógicas
             VetorDeNotas = self.Notas.copy()
-            CopiaDoValorASerSacado = ValorASerSacado
+            CopiaDeValorASerSacado = ValorASerSacado
             SaqueInvalido = False
-            print(CopiaDoValorASerSacado)
+            # Começa a distribuição das notas de cima para baixo, o motivo disso está explicado no readme.md
             for ValorDaNota in reversed(VetorDeNotas):
+                # Caso o valor a ser sacado seja maior que a nota sendo destribuída, passa para a próxima nota
                 if ValorDaNota > ValorASerSacado:
                     continue
-                while VetorDeNotas[ValorDaNota] != 0 and CopiaDoValorASerSacado > 0:
-                    if CopiaDoValorASerSacado - ValorDaNota < 0:
+                # Verifica se ainda existem notas a serem distribuidas e se o valor já não foi alcançado
+                while VetorDeNotas[ValorDaNota] != 0 and ValorASerSacado > 0:
+                    # Invalida o saque no caso das notas não poderem completar a quantia solicitada 
+                    if ValorASerSacado - ValorDaNota < 0:
                         SaqueInvalido = True
                         break
-                    CopiaDoValorASerSacado -= ValorDaNota 
+                    # Desconta cada nota do valor a ser sacado, faz o desconto na cópia do dicionário
+                    ValorASerSacado -= ValorDaNota 
                     VetorDeNotas[ValorDaNota]  = VetorDeNotas[ValorDaNota] - 1
-                    print(CopiaDoValorASerSacado)
-            for ValorDaNota in VetorDeNotas:
-                print(VetorDeNotas[ValorDaNota], "notas de", ValorDaNota)
             if SaqueInvalido:
                 print("Saque negado! Notas não permitem completar a quantia solicitada")
             else:
+                # Caso o saque seja válido, mostra as notas utilizadas 
+                print("Saque de", CopiaDeValorASerSacado, "aprovado\nAs notas ejetadas foram:")
+                for ValorDaNota in self.Notas:
+                    print(self.Notas[ValorDaNota] - VetorDeNotas[ValorDaNota], "notas de", ValorDaNota)
+                # Transfere os valores da cópia para o dicionário original 
                 self.Notas = VetorDeNotas
-
-            
-
-
-
-
-                
-            
-
-
-
-            
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,26 +1,52 @@
+# Exercício de programação - PS SAAS
+# Autor: Lucas Paiva da Silva
 
+# Subsistema hipotético de gerenciamento de caixas eletrônicos
+# Responsável por contabilizar e liberar as notas existentes no caixa eletrônico
+
+# Importa a classe CaixaEletronico, localizada no arquivo caixaeletronico.py
 from caixaeletronico import CaixaEletronico
 
+# Criação do Caixa01
 Caixa01 = CaixaEletronico()
 
-while True:
-    print('1.Sacar\n2.Abastecer o caixa\n3.Consultar dinheiro em caixa\n4.Sair')
+# Saldo ficitio para testar o sistema
+SaldoEmConta = 1000
+
+# Variável de controle do loop do menu
+ControleDoMenu = True
+
+# Loop de escolha
+while ControleDoMenu:
+    print("")
+    print('Bem vindo ao sistema bancario\n1.Sacar\n2.Abastecer o caixa\n3.Consultar dinheiro em caixa\n4.Sair')
     EscolhaDoUsuario = int(input("Sua escolha: "))
+
+    # 1 -> Saque (verificação do saldo em conta e saque)
     if EscolhaDoUsuario == 1:
         print("Você escolheu efetuar um saque")
-        Caixa01.sacarNotas(int(input("Quanto você gostaria de sacar? ")))
+        ValorASerSacado = int(input("Quanto você gostaria de sacar? "))
+        # Verifica se o usuário possúi o valor a ser sacado na connta 
+        if ValorASerSacado>SaldoEmConta:
+            print("Você não possuí saldo suficiente em conta para realizar esse saque!")
+            continue
+        Caixa01.sacarNotas(ValorASerSacado)
 
+    # 2 -> Deposito (abasteciomento do caixa )
     elif EscolhaDoUsuario == 2:
         print("Você escolheu abastercer o caixa")
-        Caixa01.carregarNotas(int(input("Valor da nota que será inserida: ")), int(input("Quantidade de notas a serem inseridas: ")))
+        Caixa01.carregarNotas(int(input("Valor da(s) nota(s): ")), int(input("Quantidade de nota(s) a serem inseridas: ")))
 
+    # 3 -> Consulta dos valores em caixa
     elif EscolhaDoUsuario == 3:
         print("O valor em caixa é de: ", Caixa01.retornarValorEmCaixa())
         print("Disposto nas seguintes notas")
         Caixa01.mostrarCedulasEmCaixa()
 
-    else:
+    # 4 -> Sair e finalizar o programa
+    elif EscolhaDoUsuario == 4:
         print("Você escolheu sair!")
-        break
+        ControleMenu = False
     
-    print("\n")
+    else:
+        print("Entrada inválida, selecione 1,2,3 ou 4")
